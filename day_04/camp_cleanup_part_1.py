38d67d63a98d93b986074750505aec7a66810579
Menhaj Ahmed <menhajahmed@gmail.com>
2022-12-04 13:00:36 +0000

    Day 4 solution

---
+++ day_04/camp_cleanup_part_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from re import split
+
+fully_contained_pairs = 0
+with open("input.txt", encoding="utf-8") as input:
+    for line in input:
+        assignments = list(map(int, split("[-,]", line)))
+        if assignments[0] <= assignments[2]:
+            a_section_start, a_section_end, b_section_start, b_section_end = assignments
+        else:
+            b_section_start, b_section_end, a_section_start, a_section_end = assignments
+        if a_section_start == b_section_start or b_section_end <= a_section_end:
+            fully_contained_pairs += 1
+print(fully_contained_pairs)
